c855a5ebb213bc9952c1dea090481ae2e03df856
Peter McGarrigle <mcgarrigle@github.com>
2024-06-30 14:47:34 +0000

    convert login to JSON

--- flows/normalisation/normalisation/login.py
+++ flows/normalisation/normalisation/login.py
@@ -1,3 +1,12 @@
+import re
+import json
 
 def normalise_login(event):
-    return "normal " + event
+    pattern = r'(.{19}) (\w+) (.+)'
+    match = re.match(pattern, event)
+    if match:
+        timestamp, process, message = match.groups()
+        rec = { "timestamp": timestamp, "process": process, "message": message }
+        return json.dumps(rec)
+    else:
+        return None
