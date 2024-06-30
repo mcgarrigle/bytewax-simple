ce2d90b09bfd600e72504df4ef4ca497b5c131f3
Peter McGarrigle <mcgarrigle@github.com>
2024-06-30 09:07:17 +0000

    added kafka.sh management tool

---
+++ flows/kafka/kafka.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# docker exec -it kafka-broker-1 bash
+# /opt/kafka/bin/kafka-console-producer.sh --bootstrap-server broker:19092 --topic raw
+
+function run {
+  docker exec -it kafka-broker-1 $@
+}
+
+function kafka {
+  run /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server ${KAFKA_BOOTSTRAP} --topic raw
+}
+
+# kafka.sh consumer --topic raw --from-beginning
+
+function command_consumer {
+  run /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server ${KAFKA_BOOTSTRAP} $@
+}
+
+# kafka.sh producer --topic raw
+
+function command_producer {
+  run /opt/kafka/bin/kafka-console-producer.sh --bootstrap-server ${KAFKA_BOOTSTRAP} $@
+}
+
+# kafka.sh topics --create --topic raw
+
+function command_topics {
+  run /opt/kafka/bin/kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP} $@
+}
+
+command=$1
+shift
+command_$command $@
--- flows/normalisation/README.md
+++ flows/normalisation/README.md
@@ -1,6 +1,6 @@
 ## Instructions
 
-Setup kafka broker (see ../kafka/README)
+Setup kafka broker (see ../kafka/README.md)
 ```
 . deploy.env
 docker compose -f flow.compose.yml up
