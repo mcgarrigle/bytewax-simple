d4408485ebd093a99b636daa2acb9e0ad411937f
Peter McGarrigle <mcgarrigle@github.com>
2024-07-02 20:23:42 +0000

    put event functions in own namespace

--- flows/normalisation/flow.py
+++ flows/normalisation/flow.py
@@ -1,13 +1,13 @@
 import os
-import confluent_kafka
 import json
+import confluent_kafka
 
 from bytewax.dataflow import Dataflow
 from bytewax.connectors.kafka import KafkaSource, KafkaSink, SerializedKafkaSinkMessage
 import bytewax.operators as op
 
-import login
-import system
+import event.login
+import event.system
 
 brokers   = os.environ["KAFKA_BROKERS"].split(",")
 flow      = Dataflow("normalisation")
@@ -17,10 +17,10 @@ sink      = KafkaSink(brokers=brokers, topic="normalised")
 
 stream    = op.input("in", flow, source)
 events    = op.map("extract", stream, lambda x: x.value.decode("utf-8"))
-logins    = op.filter("filter_logins", events, login.recognise)
-systems   = op.filter("filter_systems", events, system.recognise)
-logins_n  = op.map("normalise_logins", logins, login.normalise)
-systems_n = op.map("normalise_systems", systems, system.normalise)
+logins    = op.filter("filter_logins", events, event.login.recognise)
+systems   = op.filter("filter_systems", events, event.system.recognise)
+logins_n  = op.map("normalise_logins", logins, event.login.normalise)
+systems_n = op.map("normalise_systems", systems, event.system.normalise)
 combined  = op.merge("merge", logins_n, systems_n)
 out       = op.map("message", combined, lambda x: SerializedKafkaSinkMessage(key=None, value=json.dumps(x)))
 
