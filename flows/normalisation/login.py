a0459165316a5f7fba4418e1ccfd0016957a708c
Peter McGarrigle <mcgarrigle@github.com>
2024-07-02 14:18:28 +0000

    added hostname

--- flows/normalisation/login.py
+++ flows/normalisation/login.py
@@ -2,11 +2,11 @@ import re
 import json
 
 def normalise_login(event):
-    pattern = r'(.{19}) (\w+) (.+)'
+    pattern = r'(.{19}) (\w+) (\w+) (.+)'
     match = re.match(pattern, event)
     if match:
-        timestamp, process, message = match.groups()
-        rec = { "timestamp": timestamp, "process": process, "message": message }
+        timestamp, hostname, process, message = match.groups()
+        rec = { "timestamp": timestamp, "hostname": hostname, "process": process, "message": message }
         return json.dumps(rec)
     else:
         return None
