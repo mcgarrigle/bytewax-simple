829d042bc924625f38f9b9abc8148c4108f9fb9c
Peter McGarrigle <mcgarrigle@github.com>
2024-07-02 13:34:55 +0000

    puts normalised events into kafka topic

--- flows/normalisation/flow.py
+++ flows/normalisation/flow.py
@@ -2,9 +2,8 @@ import os
 import confluent_kafka
 
 from bytewax.dataflow import Dataflow
+from bytewax.connectors.kafka import KafkaSource, KafkaSink, SerializedKafkaSinkMessage
 import bytewax.operators as op
-from bytewax.connectors.kafka import KafkaSource, KafkaSink, KafkaSinkMessage
-from bytewax.connectors.stdio import StdOutSink
 
 from login import normalise_login
 
@@ -12,8 +11,13 @@ brokers   = os.environ["KAFKA_BROKERS"].split(",")
 flow      = Dataflow("normalisation")
 
 source    = KafkaSource(brokers=brokers, topics=["raw"], starting_offset=confluent_kafka.OFFSET_BEGINNING)
+sink      = KafkaSink(brokers=brokers, topic="normalised")
+
 stream    = op.input("raw-in", flow, source)
 events    = op.map("extract-events", stream, lambda x: x.value.decode("utf-8"))
 processed = op.map("map", events, normalise_login)
+out       = op.map("out", processed, lambda x: SerializedKafkaSinkMessage(key=None, value=x))
 
-op.output("normalised-out", processed, StdOutSink())
+# op.inspect("debug", out)
+
+op.output("normalised-out", out, sink)
