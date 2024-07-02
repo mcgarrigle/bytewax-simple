5c0bbb7a75e4b9e8ae41b21e2eeac10660ba6d5f
Peter McGarrigle <mcgarrigle@github.com>
2024-07-02 10:44:18 +0000

    manage brokers via KAFKA_BROKERS variable

--- flows/normalisation/flow.py
+++ flows/normalisation/flow.py
@@ -1,17 +1,19 @@
+import os
+import confluent_kafka
+
 from bytewax.dataflow import Dataflow
 import bytewax.operators as op
-from bytewax.connectors.kafka import operators as kop
+from bytewax.connectors.kafka import KafkaSource, KafkaSink, KafkaSinkMessage
 from bytewax.connectors.stdio import StdOutSink
-
-import confluent_kafka
 
 from login import normalise_login
 
-flow = Dataflow("kafka-example")
-brokers = ["broker:19092"]
+brokers   = os.environ["KAFKA_BROKERS"].split(",")
+flow      = Dataflow("normalisation")
 
-stream = kop.input("kafka-in", flow, brokers=brokers, topics=["raw"], starting_offset=confluent_kafka.OFFSET_BEGINNING)
-events = op.map("extract-event", stream.oks, lambda x: x.value.decode("utf-8"))
+source    = KafkaSource(brokers=brokers, topics=["raw"], starting_offset=confluent_kafka.OFFSET_BEGINNING)
+stream    = op.input("raw-in", flow, source)
+events    = op.map("extract-events", stream, lambda x: x.value.decode("utf-8"))
 processed = op.map("map", events, normalise_login)
 
-op.output("out", processed, StdOutSink())
+op.output("normalised-out", processed, StdOutSink())
