49e7956af9e742fc85ff4b02a3e08f15cde7ca76
Peter McGarrigle <mcgarrigle@github.com>
2024-07-03 21:21:35 +0000

    select normaliser using branch operator

--- flows/normalisation/flow.py
+++ flows/normalisation/flow.py
@@ -17,13 +17,14 @@ sink      = KafkaSink(brokers=brokers, topic="normalised")
 
 stream    = op.input("in", flow, source)
 events    = op.map("extract", stream, lambda x: x.value.decode("utf-8"))
-logins    = op.filter("filter_logins", events, event.login.recognise)
-systems   = op.filter("filter_systems", events, event.system.recognise)
-logins_n  = op.map("normalise_logins", logins, event.login.normalise)
-systems_n = op.map("normalise_systems", systems, event.system.normalise)
+logins    = op.branch("filter_logins", events, event.login.recognise)
+systems   = op.branch("filter_systems", logins.falses, event.system.recognise)
+logins_n  = op.map("normalise_logins", logins.trues, event.login.normalise)
+systems_n = op.map("normalise_systems", systems.trues, event.system.normalise)
 combined  = op.merge("merge", logins_n, systems_n)
 out       = op.map("message", combined, lambda x: SerializedKafkaSinkMessage(key=None, value=json.dumps(x)))
 
 op.inspect("debug", out)
+op.inspect("unmatched", systems.falses)
 
 op.output("out", out, sink)
